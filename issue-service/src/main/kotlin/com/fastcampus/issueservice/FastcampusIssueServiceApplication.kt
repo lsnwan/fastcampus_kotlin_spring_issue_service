ba9bb3b232b4700e40d4b2ee439b6308b46e9287
HyeonTae <lsnwan@naver.com>
2023-07-25 22:26:26 +0900

    first commit

---
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/FastcampusIssueServiceApplication.kt
@@ -0,0 +1,14 @@
+package com.fastcampus.issueservice
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@SpringBootApplication
+@EnableJpaAuditing
+class FastcampusIssueServiceApplication
+
+fun main(args: Array<String>) {
+  runApplication<FastcampusIssueServiceApplication>(*args)
+}
---
+++ issue-service/src/main/kotlin/com/fastcampus/issueservice/web/IssueController.kt
@@ -0,0 +1,42 @@
+package com.fastcampus.issueservice.web
+
+import com.fastcampus.issueservice.config.AuthUser
+import com.fastcampus.issueservice.domain.enums.IssueStatus
+import com.fastcampus.issueservice.model.IssueRequest
+import com.fastcampus.issueservice.service.IssueService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/issues")
+class IssueController (
+  private val issueService: IssueService
+) {
+
+  @PostMapping
+  fun create(
+    authUser: AuthUser,
+    @RequestBody request: IssueRequest
+  ) = issueService.create(authUser.userId, request)
+
+
+  @GetMapping
+  fun getAll(
+    authUser: AuthUser,
+    @RequestParam(required = false, defaultValue = "TODO") status: IssueStatus,
+  ) = issueService.getAll(status)
+
+  @GetMapping("/{id}")
+  fun get(
+    authUser: AuthUser,
+    @PathVariable id: Long,
+  ) = issueService.get(id)
+
+
+
+}
